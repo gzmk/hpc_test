323b8d3ff9a07d694df276400a80de8dc605158d
gzmk <gizemkucukoglu@gmail.com>
2015-04-03 13:02:27 -0400

    added .pbs to run it on HPC

--- renderIm_hpc.m
+++ renderIm_hpc.m
@@ -60,8 +60,8 @@ montageFile = [montageName '.png'];
     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
 
 % load the monochromatic image and display it
-imPath = ['/scratch/gk925/render-toolbox/', hints.recipeName, '/renderings/Mitsuba/test_sphere-001.mat']
-load(imPath, 'multispectralImage');
-im2 = multispectralImage;
-figure;imshow(im2(:,:,1))
+% imPath = ['/scratch/gk925/render-toolbox/', hints.recipeName, '/renderings/Mitsuba/test_sphere-001.mat']
+% load(imPath, 'multispectralImage');
+% im2 = multispectralImage;
+% figure;imshow(im2(:,:,1))
 
